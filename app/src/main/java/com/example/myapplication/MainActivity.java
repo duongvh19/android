6108c6831c6e08e2004edb856e36e48982fef67d
duongvh19 <duongvh1921@gmail.com>
2022-06-01 23:58:07 +0700

    [android] introduce AsyncTask

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -2,13 +2,41 @@ package com.example.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
+    private static final String TAG = "MainActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Log.d(TAG, "onCreate: " + " starting AsyncTask");
+        DownloadData downloadData = new DownloadData();
+        downloadData.execute("URL goes here");
+        Log.d(TAG, "onCreate: " + "Done");
+    }
+
+    private class DownloadData extends AsyncTask<String, Void, String> {
+        private static final String TAG = "DownloadData";
+
+        @Override
+        protected String doInBackground(String... strings) {
+            Log.d(TAG, "doInBackground: " + "start with  " + strings[0]);
+            return "doInBackground completed!";
+        }
+
+        @Override
+        protected void onProgressUpdate(Void... values) {
+            super.onProgressUpdate(values);
+        }
+
+        @Override
+        protected void onPostExecute(String s) {
+            Log.d(TAG, "onPostExecute: " + "parameter is " + s);
+            super.onPostExecute(s);
+        }
     }
 }
