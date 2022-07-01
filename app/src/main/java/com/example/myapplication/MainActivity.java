635396d00f8115f5a441d653bafabc4b8029dc4e
duongvh19 <duongvh1921@gmail.com>
2022-07-01 21:01:41 +0700

    [android] api youtube app

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -2,13 +2,41 @@ package com.example.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button btnPlaySingle = findViewById(R.id.btnPlaySingle);
+        Button btnStandalone = findViewById(R.id.btnStandalone);
+
+        btnPlaySingle.setOnClickListener(this);
+        btnStandalone.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        Intent intent = null;
+
+        switch (v.getId()) {
+            case R.id.btnPlaySingle:
+                intent = new Intent(this, YoutubeActivity.class);
+                break;
+            case R.id.btnStandalone:
+                intent = new Intent(this, StandaloneActivity.class);
+                break;
+            default:
+        }
+
+        if (intent != null) {
+            startActivity(intent);
+        }
     }
 }
